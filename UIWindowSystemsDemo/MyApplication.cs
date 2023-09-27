5c66560e228441c9cd5635d31fe9c55586aedc0e
Daniel Cáceres <daniel.caceresm@outlook.com>
2023-09-27 12:44:42 +0200

    Update latest release

--- UIWindowSystemsDemo/MyApplication.cs
+++ UIWindowSystemsDemo/MyApplication.cs
@@ -9,15 +9,15 @@ namespace UIWindowSystemsDemo
     {
         public MyApplication()
         {
-            this.Container.RegisterType<Clock>();
-            this.Container.RegisterType<TimerFactory>();
-            this.Container.RegisterType<Random>();
-            this.Container.RegisterType<ErrorHandler>();
-            this.Container.RegisterType<ScreenContextManager>();
-            this.Container.RegisterType<GraphicsPresenter>();
-            this.Container.RegisterType<AssetsDirectory>();
-            this.Container.RegisterType<AssetsService>();
-            this.Container.RegisterType<ForegroundTaskSchedulerService>();
+            this.Container.Register<Clock>();
+            this.Container.Register<TimerFactory>();
+            this.Container.Register<Random>();
+            this.Container.Register<ErrorHandler>();
+            this.Container.Register<ScreenContextManager>();
+            this.Container.Register<GraphicsPresenter>();
+            this.Container.Register<AssetsDirectory>();
+            this.Container.Register<AssetsService>();
+            this.Container.Register<ForegroundTaskSchedulerService>();
         }
 
         public override void Initialize()
